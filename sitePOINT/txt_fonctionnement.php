9f9ddf413e88081601481fbe58814504d4ee62a4
ayapsoun <77096662+ayanajare@users.noreply.github.com>
2022-04-03 20:11:52 +0200

    Delete sitePOINT directory

--- sitePOINT/txt_fonctionnement.php
+++
@@ -1,8 +0,0 @@
-<!--Texte affiché par la page de présentation du robot-->
-
-<div>
-	<H4>1. Concept de base</H4>
-	<p>
-		Nieh le robot est pas fini, on fera cette partie quand sa sera le cas...
-	</p>
-</div>
